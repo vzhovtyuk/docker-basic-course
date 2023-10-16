5bd86b05687edcaefec231f4ab7978d40fd9caa5
vkazhdan <vkazhdan@anotheria.net>
2023-10-16 16:19:21 +0300

    4_mysql_environment_variables

---
+++ 4_mysql_environment_variables/src/2_run_mysql_in_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d -p 3306:3306 mysql
---
+++ 4_mysql_environment_variables/src/5_6_run_mysql.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run \
+-d \
+-p 3306:3306 \
+-e MYSQL_ROOT_PASSWORD=password1234 \
+-e MYSQL_DATABASE=my_main_db \
+-v ~/local-directory:/var/lib/mysql \
+--name mysqlserver \
+mysql
